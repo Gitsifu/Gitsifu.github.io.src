3486727852b6cc60cf10d9d5f981b447c15e5b4e
sifu <2587107273@qq.com>
2023-10-18 14:29:48 +0800

    update

--- blog/.vuepress/enhanceApp.js
+++ blog/.vuepress/enhanceApp.js
@@ -8,32 +8,27 @@ import DemoFather from './demo/on-emit/demo-father'
 import VueBottomSheetVue2Demo from "./demo/vue-bottom-sheet-vue2/vue-bottom-sheet-vue2-demo";
 import Watermark from './demo/watermark'
 import RenderDocx from './demo/render-docx'
-export default ({
-                    Vue, // VuePress 正在使用的 Vue 构造函数
-                    options, // 附加到根实例的一些选项
-                    router, // 当前应用的路由实例
-                    siteData, // 站点元数据
-                    isServer // 当前应用配置是处于 服务端渲染 或 客户端
-                }) => {
-    Vue.component(SomeDemo.name, SomeDemo)
-    Vue.component(FontZpix.name, FontZpix)
-    Vue.component(RichText.name, RichText)
-    Vue.component(VAnimated.name, VAnimated)
-    Vue.component(OnEmit.name, OnEmit)
-    Vue.component(DemoChild.name, DemoChild)
-    Vue.component(DemoFather.name, DemoFather)
-    Vue.component(VueBottomSheetVue2Demo.name, VueBottomSheetVue2Demo)
-    Vue.component(Watermark.name, Watermark)
-    Vue.component(RenderDocx.name, RenderDocx)
+import copy from "./utils/copy";
+import tongji from "./utils/tongji";
 
-    // 路由切换事件处理
-    router.beforeEach((to, from, next) => {
-      //触发百度的pv统计
-      if (typeof _hmt != "undefined") {
-        if (to.path) {
-          _hmt.push(["_trackPageview", to.fullPath]);
-        }
-      }
-      next();
-    });
+export default ({
+                  Vue, // VuePress 正在使用的 Vue 构造函数
+                  options, // 附加到根实例的一些选项
+                  router, // 当前应用的路由实例
+                  siteData, // 站点元数据
+                  isServer // 当前应用配置是处于 服务端渲染 或 客户端
+                }) => {
+  Vue.component(SomeDemo.name, SomeDemo)
+  Vue.component(FontZpix.name, FontZpix)
+  Vue.component(RichText.name, RichText)
+  Vue.component(VAnimated.name, VAnimated)
+  Vue.component(OnEmit.name, OnEmit)
+  Vue.component(DemoChild.name, DemoChild)
+  Vue.component(DemoFather.name, DemoFather)
+  Vue.component(VueBottomSheetVue2Demo.name, VueBottomSheetVue2Demo)
+  Vue.component(Watermark.name, Watermark)
+  Vue.component(RenderDocx.name, RenderDocx)
+
+  Vue.use(copy)
+  tongji(router)
 }
